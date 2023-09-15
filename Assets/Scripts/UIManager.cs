1d3605eab16c73c9514b2ae42caa1995fe849ea4
miran2511 <kimmiran2511@gmail.com>
2023-09-15 11:27:24 +0800

    init

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public Button restartButton;
+    public PlayerMovement playerMovement;
+    public TextMeshProUGUI scoreText;
+    public JumpOverGoomba jumpOverGoomba;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerMovement.isGameOver)
+        {
+            restartButton.gameObject.SetActive(false);
+            scoreText.enabled = false;
+            gameOverPanel.SetActive(true);
+        }
+    }
+}
